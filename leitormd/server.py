cf286444d831b4abafd1fe1f2ca1e85addeabe9f
Gonçalo Valério <gon@ovalerio.net>
2016-01-09 19:59:58 +0000

    added simple tree / dir transversal

--- leitormd/server.py
+++ leitormd/server.py
@@ -1,16 +1,42 @@
 from flask import Flask
 import webbrowser
+import os
+import json
+
+ALLOWED_IMAGE_EXTENSIONS = ["gif", "jpeg", "jpg", "bmp", "png"]
 
 app = Flask(__name__)
-app.file_tree = {}
 
 
 # Initialization functions
-def build_tree():
-    pass
+def build_tree(rootDir='.'):
+    for dirName, subdirList, fileList in os.walk(rootDir):
+        dir_contents = get_dir_contents(fileList)
+        path_nodes = dirName.split("/") if dirName != "." else []
+        if path_nodes:
+            own_node = path_nodes[-1]
+            get_parent(path_nodes)[own_node] = dir_contents
+        else:
+            app.file_tree = dir_contents
 
 
-def run_server(open_browser=False):
+def get_parent(path_nodes):
+    current_parent = app.file_tree
+    for parent in path_nodes[1:-1]:
+        current_parent = current_parent[parent]
+    return current_parent
+
+
+def get_dir_contents(file_list):
+    content = {}
+    for fname in file_list:
+        name_components = fname.split(".")
+        extension = name_components[-1] if len(name_components) > 1 else None
+        content[fname] = {"extension": extension}
+    return content
+
+
+def run_server(open_browser=True):
     if open_browser:
         browser = webbrowser.get()
         browser.open("http://localhost:5000")
@@ -23,9 +49,9 @@ def index():
     return "Hello World!"
 
 
-@app.route("/:file:")
+@app.route("/<path:file>")
 def show(file):
-    return "page"
+    return "true"
 
 
 # Entry points
